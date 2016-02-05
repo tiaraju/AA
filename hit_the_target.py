f940110b36992dfc006bd842a4c2cf37ddf32274
Tiaraju Smaneoto <tiaraju.smaneoto@ccc.ufcg.edu.br>
2016-02-05 17:44:36 -0300

    hit the target in braile - almost done

--- hit_the_target.py
+++ hit_the_target.py
@@ -1,30 +1,31 @@
 # -*- coding: utf-8 -*-
  
-quantidadeDeParedes = int(raw_input())
-possivelCoeficiente = []
+n = int(raw_input())
+coef = []
 coefAngA, coefAngB = 0, 0
+hittable = True
  
-for i in range(quantidadeDeParedes+1):
+for i in range(n+1):
 	parede = map(int, raw_input().split())
 	distancia = parede[0]
 	alturaA = parede[1]
 	alturaB = parede[2]
 	coefAngA = (float(alturaA))/distancia
 	coefAngB = (float(alturaB))/distancia
-	possivelCoeficiente.append([coefAngA, coefAngB])
+	coef.append([coefAngA, coefAngB])
  
-possivel = True
-for i in range(1, len(possivelCoeficiente)):
-	if (possivelCoeficiente[i][0] > possivelCoeficiente[i-1][1]) or (possivelCoeficiente[i][1] < possivelCoeficiente[i-1][0]):
-		possivel = False
+
+for i in range(1, len(coef)):
+	if (coef[i][0] > coef[i-1][1]) or (coef[i][1] < coef[i-1][0]):
+		hittable = False
 		break
 	else:
-		if (possivelCoeficiente[i][0] < possivelCoeficiente[i-1][0]):
-			possivelCoeficiente[i][0] = possivelCoeficiente[i-1][0]
-		if (possivelCoeficiente[i][1] > possivelCoeficiente[i-1][1]):
-			possivelCoeficiente[i][1] = possivelCoeficiente[i-1][1]
+		if (coef[i][0] < coef[i-1][0]):
+			coef[i][0] = coef[i-1][0]
+		if (coef[i][1] > coef[i-1][1]):
+			coef[i][1] = coef[i-1][1]
  
-if possivel:
+if hittable:
 	print "Y"
 else:
 	print "N"
--- in_braille.py
+++ in_braille.py
@@ -1,21 +1,51 @@
-number_to_braille = {0:".*\n**\n..",1:"*.\n..\n..",2:"*.\n*.\n..",3:"**\n..\n..",4:"**\n.*\n..",5:"*.\n.*\n..",6:"**\n*.\n..",7:"**\n**\n..",8:"*.\n**\n..",9:".*\n*.\n.."}
+first_number_to_braille = {0:". *",1:"* .",2:"* .",3:"* *",4:"* *",5:"* .",6:"* *",7:"* *",8:"* .",9:". *"}
+second_number_to_braille = {0:"* *",1:". .",2:"* .",3:". .",4:". *",5:". *",6:"* .",7:"* *",8:"* *",9:"* ."}
 
-braille_to_number = {".*\n**\n..":0,"*.\n..\n..":1,"*.\n*.\n..":2,"**\n..\n..":3,"**\n.*\n..":4,"*.\n.*\n..":5,"**\n*.\n..":6,"**\n**\n..":7,"*.\n**\n..":8,".*\n*.\n..":9}
+braille_to_number = {".***..":0,"*.....":1,"*.*...":2,"**....":3,"**.*..":4,"*..*..":5,"***...":6,"****..":7,"*.**..":8,".**...":9}
 
+word=[]
 
 
 d = int(raw_input())
 while(d != 0):
 	l = raw_input()
+	result =""
 	if(l=="S"):
 		number = int(raw_input())
-		result =""
 		for i in str(number):
-			result+=number_to_braille[int(i)]
-		print(result)
-	else:
-		print("TODO")
+			result+=(first_number_to_braille[int(i)])
+			if(i < len(str(number))-1):
+				result+=" "
+		result+="\n"
 
+		for i in str(number):
+			result+=(second_number_to_braille[int(i)])
+			if(i < len(str(number))-1):
+				result+=" "
+		result+="\n"
+
+		for i in str(number):
+			result+=". ."
+			if(i < len(str(number))-1):
+				result+=" "
+
+	else:
+		digit = ""
+		for i in range(3):
+			j=i
+			line = raw_input().split()
+			for k in range(len(line)):
+				word.insert(j,line[k])
+				j+=3
+
+		for i in range(len(word)):
+			if(i%3 == 0 and i!=0):
+				result+=str(braille_to_number.get(digit))
+				digit=word[i]
+			else:
+				digit+=word[i]
+
+	print result
 	d = int(raw_input())
 
 
